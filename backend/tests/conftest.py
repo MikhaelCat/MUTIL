b57ff9344912a56a348e6055e45c101ad070462d
Mikhael <opanasukmihail80@gmail.com>
2025-08-14 13:38:39 +1000

    Update conftest.py

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,10 +1,19 @@
+import os
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.core.database import Base
-from app.core.config import settings
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+os.environ['POSTGRES_USER'] = 'test_user'
+os.environ['POSTGRES_PASSWORD'] = 'test_password'
+os.environ['POSTGRES_DB'] = 'test_db'
+os.environ['DATABASE_URL'] = "sqlite:///./test.db"
+os.environ['REDIS_URL'] = "redis://localhost:6379/0"
+os.environ['SECRET_KEY'] = "test-secret-key-for-tests"
+
+
+from app.core.config import settings
+SQLALCHEMY_DATABASE_URL = settings.database_url # Это будет "sqlite:///./test.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
