aaee943d3bb0f04600e630e7affa06ca3ce0e31d
Mikhael <opanasukmihail80@gmail.com>
2025-08-15 17:56:38 +1000

    Update database.py

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,5 +1,4 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import declarative_base, sessionmaker
 from app.core.config import settings
 
@@ -13,6 +12,4 @@ def get_db():
     try:
         yield db
     finally:
-
         db.close()
-
